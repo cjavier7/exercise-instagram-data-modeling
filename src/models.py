57c21a12af241174272d97c4624ffb65c5b99640
Carlos J Guzman <152306129+cjavier7@users.noreply.github.com>
2024-02-10 16:22:38 +0000

    terminado

--- src/models.py
+++ src/models.py
@@ -7,7 +7,7 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
+""" class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
@@ -26,7 +26,58 @@ class Address(Base):
     person = relationship(Person)
 
     def to_dict(self):
-        return {}
+        return {} """
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String (200), nullable=False)
+    first_name = Column(String(200), nullable=False)
+    last_name = Column(String(200), nullable=False)
+    email = Column(String(200), unique=True, nullable=False)
+    comment = relationship('Comment')
+    post = relationship('Post')
+
+
+class Comment(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(200), nullable=False)
+    author_id = Column(Integer, ForeignKey('users.id'))
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    user = relationship('User')
+    post = relationship('Post')
+
+class Post(Base):
+    __tablename__ = 'posts'
+    id= Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    comments = relationship("Comment")
+    media = relationship('Media')
+    
+class Media(Base):
+    __tablename__ = 'medias'
+    id = Column(Integer, primary_key=True)
+    type = Column(String (200), nullable=False)
+    url = Column(String(200), nullable=False)
+    post_id= Column(Integer,  ForeignKey('posts.id'))
+    post = relationship('Post')
+
+class Follower(Base):
+    __tablename__ = 'followers'
+    user_from_id= Column(Integer, ForeignKey('users.id'), primary_key=True)
+    user_to_id= Column(Integer, ForeignKey('users.id'), primary_key=True)
+    user = relationship('User')
+
+
+
+
+
+
+
+
+
+
 
 ## Draw from SQLAlchemy base
 try:
